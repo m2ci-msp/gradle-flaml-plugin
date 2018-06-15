65879599a58c68381d9a0efefd4c77460dae7b34
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-06-15 17:08:44 +0200

    tweak classes

--- src/main/groovy/org/m2ci/msp/flaml/ExtractLab.groovy
+++ src/main/groovy/org/m2ci/msp/flaml/ExtractLab.groovy
@@ -1,11 +1,12 @@
 package org.m2ci.msp.flaml
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.*
-
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
 import org.yaml.snakeyaml.Yaml
 
 class ExtractLab extends DefaultTask {
+
     @OutputDirectory
     def destDir = project.file("$project.buildDir/lab")
 
