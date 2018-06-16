e3399dc9daf8cf454554fe7adfb9c9ba95003533
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-06-16 14:11:35 +0200

    provide, unpack test buildscript, test plugin

--- src/test/groovy/org/m2ci/msp/flaml/FlamlPluginFunctionalTest.groovy
+++ src/test/groovy/org/m2ci/msp/flaml/FlamlPluginFunctionalTest.groovy
@@ -13,12 +13,24 @@ class FlamlPluginFunctionalTest {
     void setup() {
         def projectDir = File.createTempDir()
         gradle = GradleRunner.create().withPluginClasspath().withProjectDir(projectDir)
+        ['build.gradle'].each { resourceName ->
+            new File(projectDir, resourceName).withOutputStream { stream ->
+                stream << this.getClass().getResourceAsStream(resourceName)
+            }
+        }
     }
 
     @Test
     void canRun() {
-        def result = gradle.build()
+        def result = gradle.withArguments().build()
         println result.output
         assert result.task(':help').outcome == TaskOutcome.SUCCESS
+    }
+
+    @Test
+    void canApplyPlugin() {
+        def result = gradle.withArguments(':hasPlugin').build()
+        println result.output
+        assert result.task(':hasPlugin').outcome == TaskOutcome.SUCCESS
     }
 }
