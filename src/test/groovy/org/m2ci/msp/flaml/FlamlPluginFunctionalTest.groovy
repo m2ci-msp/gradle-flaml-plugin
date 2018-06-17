7964c2094868960f1ca65bbb57a8d35a5b21eb11
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-06-17 15:57:41 +0200

    test lab extraction
    TODO: compare parsed, not raw, content

--- src/test/groovy/org/m2ci/msp/flaml/FlamlPluginFunctionalTest.groovy
+++ src/test/groovy/org/m2ci/msp/flaml/FlamlPluginFunctionalTest.groovy
@@ -27,7 +27,8 @@ class FlamlPluginFunctionalTest {
                 ['help'],
                 ['hasPlugin'],
                 ['hasTestResources'],
-                ['testExtractWav']
+                ['testExtractWav'],
+                ['testExtractLab']
         ]
     }
 
