e12613f9afe8323084bba24dcd6760aaa7f9617d
Ingmar Steiner <steiner@coli.uni-saarland.de>
2018-07-19 10:45:05 +0200

    functional test for FLAC generation

--- src/test/groovy/org/m2ci/msp/flaml/FlamlPluginFunctionalTest.groovy
+++ src/test/groovy/org/m2ci/msp/flaml/FlamlPluginFunctionalTest.groovy
@@ -49,6 +49,7 @@ class FlamlPluginFunctionalTest {
     @DataProvider
     Object[][] generationTasks() {
         [
+                ['testGenerateFlac'],
                 ['testInjectText'],
                 ['testInjectSegments']
         ]
