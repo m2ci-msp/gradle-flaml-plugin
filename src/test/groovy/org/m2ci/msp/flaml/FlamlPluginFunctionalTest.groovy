369f775c5119482f2f2567786b685485eab8a516
Ingmar Steiner <steiner@coli.uni-saarland.de>
2022-11-26 19:37:57 +0100

    Refactor test to fix Gradle errors, deprecations

--- src/test/groovy/org/m2ci/msp/flaml/FlamlPluginFunctionalTest.groovy
+++ src/test/groovy/org/m2ci/msp/flaml/FlamlPluginFunctionalTest.groovy
@@ -2,7 +2,7 @@ package org.m2ci.msp.flaml
 
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
-import org.testng.annotations.BeforeClass
+import org.testng.annotations.BeforeGroups
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 
@@ -10,12 +10,47 @@ class FlamlPluginFunctionalTest {
 
     GradleRunner gradle
 
-    @BeforeClass
-    void setup() {
+    @BeforeGroups(groups = 'core')
+    void setupCore() {
         def projectDir = File.createTempDir()
+        new File(projectDir, 'settings.gradle').createNewFile()
+        new File(projectDir, 'build.gradle').withWriter {
+            it << this.class.getResourceAsStream('build-core.gradle')
+        }
         gradle = GradleRunner.create().withPluginClasspath().withProjectDir(projectDir)
-        ['build-core.gradle', 'build-extraction.gradle', 'build-generation.gradle',
-         'foobar.flac', 'foobar.yaml', 'foobar.wav', 'foobar.TextGrid', 'foo_padded.wav', 'baz_padded.wav',
+        ['foobar.flac', 'foobar.yaml', 'foobar.wav', 'foobar.TextGrid', 'foo_padded.wav', 'baz_padded.wav',
+         'foo.wav', 'bar.wav', 'baz.wav', 'foo.lab', 'baz.lab', 'foo.txt', 'baz.txt'].each { resourceName ->
+            new File(projectDir, resourceName).withOutputStream { stream ->
+                stream << this.getClass().getResourceAsStream(resourceName)
+            }
+        }
+    }
+
+    @BeforeGroups(groups = 'extraction')
+    void setupExtraction() {
+        def projectDir = File.createTempDir()
+        new File(projectDir, 'settings.gradle').createNewFile()
+        new File(projectDir, 'build.gradle').withWriter {
+            it << this.class.getResourceAsStream('build-extraction.gradle')
+        }
+        gradle = GradleRunner.create().withPluginClasspath().withProjectDir(projectDir)
+        ['foobar.flac', 'foobar.yaml', 'foobar.wav', 'foobar.TextGrid', 'foo_padded.wav', 'baz_padded.wav',
+         'foo.wav', 'bar.wav', 'baz.wav', 'foo.lab', 'baz.lab', 'foo.txt', 'baz.txt'].each { resourceName ->
+            new File(projectDir, resourceName).withOutputStream { stream ->
+                stream << this.getClass().getResourceAsStream(resourceName)
+            }
+        }
+    }
+
+    @BeforeGroups(groups = 'generation')
+    void setupGeneration() {
+        def projectDir = File.createTempDir()
+        new File(projectDir, 'settings.gradle').createNewFile()
+        new File(projectDir, 'build.gradle').withWriter {
+            it << this.class.getResourceAsStream('build-generation.gradle')
+        }
+        gradle = GradleRunner.create().withPluginClasspath().withProjectDir(projectDir)
+        ['foobar.flac', 'foobar.yaml', 'foobar.wav', 'foobar.TextGrid', 'foo_padded.wav', 'baz_padded.wav',
          'foo.wav', 'bar.wav', 'baz.wav', 'foo.lab', 'baz.lab', 'foo.txt', 'baz.txt'].each { resourceName ->
             new File(projectDir, resourceName).withOutputStream { stream ->
                 stream << this.getClass().getResourceAsStream(resourceName)
@@ -57,27 +92,27 @@ class FlamlPluginFunctionalTest {
         ]
     }
 
-    @Test(dataProvider = 'coreTasks')
+    @Test(groups = 'core', dataProvider = 'coreTasks')
     void testCoreTasks(String taskName) {
-        def result = gradle.withArguments('--build-file', 'build-core.gradle',
+        def result = gradle.withArguments(
                 '--warning-mode', 'all',
                 taskName).build()
         println result.output
         assert result.task(":$taskName").outcome in [TaskOutcome.SUCCESS, TaskOutcome.UP_TO_DATE]
     }
 
-    @Test(dataProvider = 'extractionTasks')
+    @Test(groups = 'extraction', dataProvider = 'extractionTasks')
     void testExtractionTasks(String taskName) {
-        def result = gradle.withArguments('--build-file', 'build-extraction.gradle',
+        def result = gradle.withArguments(
                 '--warning-mode', 'all',
                 taskName).build()
         println result.output
         assert result.task(":$taskName").outcome in [TaskOutcome.SUCCESS, TaskOutcome.UP_TO_DATE]
     }
 
-    @Test(dataProvider = 'generationTasks')
+    @Test(groups = 'generation', dataProvider = 'generationTasks')
     void testGenerationTasks(String taskName) {
-        def result = gradle.withArguments('--build-file', 'build-generation.gradle',
+        def result = gradle.withArguments(
                 '--warning-mode', 'all',
                 taskName).build()
         println result.output
